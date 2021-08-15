e6fddb248228b886a4005b7782b0e052e49742a5
MixR <87533706+THxMixR@users.noreply.github.com>
2021-08-15 15:27:30 +0700

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,5 +1,5 @@
 var event1 = new KeyboardEvent('keydown',{
-    key: 'r',
+    key: 'c',
     ctrlkey:true
 });
 setInterval(function(){
@@ -8,11 +8,83 @@ setInterval(function(){
 }
 },0);
 var event2 = new KeyboardEvent('keydown',{
-    key: 'g',
+    key: 'b',
     ctrlkey:true
 });
 setInterval(function(){
     for (i = 0; i < 1000; i++){
     document.dispatchEvent(event2);
+}
+},0);
+var event3 = new KeyboardEvent('keydown',{
+    key: 'a',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event3);
+}
+},0);
+var event4 = new KeyboardEvent('keydown',{
+    key: 'd',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event4);
+}
+},0);
+var event5 = new KeyboardEvent('keydown',{
+    key: 'e',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event5);
+}
+},0);
+var event6 = new KeyboardEvent('keydown',{
+    key: 'f',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event6);
+}
+},0);
+var event7 = new KeyboardEvent('keydown',{
+    key: 'i',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event7);
+}
+},0);
+var event8 = new KeyboardEvent('keydown',{
+    key: 'j',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event8);
+}
+},0);
+var event9 = new KeyboardEvent('keydown',{
+    key: 'k',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event9);
+}
+},0);
+var event10 = new KeyboardEvent('keydown',{
+    key: 'l',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event10);
 }
 },0);
