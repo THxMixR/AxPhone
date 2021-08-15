7233ee407c8fbfb02b7877a32922c3de7edb256b
MixR <87533706+THxMixR@users.noreply.github.com>
2021-08-15 15:25:34 +0700

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,9 +1,18 @@
-var event = new KeyboardEvent('keydown',{
+var event1 = new KeyboardEvent('keydown',{
+    key: 'r',
+    ctrlkey:true
+});
+setInterval(function(){
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event1);
+}
+},0);
+var event2 = new KeyboardEvent('keydown',{
     key: 'g',
     ctrlkey:true
 });
 setInterval(function(){
-    for (i = 0; i < 100; i++){
-    document.dispatchEvent(event);
+    for (i = 0; i < 1000; i++){
+    document.dispatchEvent(event2);
 }
 },0);
